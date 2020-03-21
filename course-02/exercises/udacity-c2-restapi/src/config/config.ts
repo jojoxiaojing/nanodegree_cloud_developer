edb927fdee5dfade0c36f32ca2e80a5919314f91
Xiaojing <jojoxiaojing@icloud.com>
2020-03-21 00:59:44 -0700

    original code

---
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "udagramjojodev",
+    "password": "Cptbtptp169$",
+    "database": "udagramjojodev",
+    "host": "udagramjojodev.czntjksspruu.us-east-2.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "",
+    "aws_media_bucket": "udagramjojodev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
